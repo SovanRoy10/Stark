9d1d69e6bc6de50d182b31aa20dc913d0d70d088
Sovan Roy <roysovan00@gmail.com>
2024-05-04 09:09:41 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  
+
+  return (
+    <>
+      <Button className='ibm-plex-sans-regular'>Click me</Button>
+    </>
+  )
+}
+
+export default App
