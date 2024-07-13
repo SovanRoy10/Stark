34cf2a1650ac57d75504931cce7b6bddb81ac784
Sovan Roy <roysovan00@gmail.com>
2024-07-13 15:46:49 +0530

    added notfound page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,13 @@ import Conversation from "./page/conversation/Conversation";
 import CodePage from "./page/code/Code";
 import Image from "./page/image/Image";
 import Music from "./page/music/Music";
-import Video from "./page/video/Video"
+import Video from "./page/video/Video";
 import SignIn from "./page/signIn/SignIn";
 import SignUp from "./page/signUp/SignUp";
 
 import { Layout } from "./page/dashboard/Layout";
 import ProtectedRoute from "./components/ProtectedRoute";
+import NotFound from "./page/Notfound";
 
 function App() {
   const router = createBrowserRouter(
@@ -74,6 +75,7 @@ function App() {
           />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </>
     )
@@ -82,4 +84,3 @@ function App() {
   return <RouterProvider router={router} />;
 }
 export default App;
-
